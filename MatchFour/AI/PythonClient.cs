0cf71faa0d96dcc312e9d1f29fa06b899c61ccfa
Alex Stiller <alex.stiller@posteo.de>
2025-11-11 15:35:27 +0100

    * add first connection test to Python RL

---
+++ MatchFour/AI/PythonClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MatchFour.AI
+{
+    public class PythonReply
+    {
+        public string reply {  get; set; }
+    }
+
+    internal class PythonClient
+    {
+        private readonly HttpClient client = new();
+
+        public async Task<string> SendTextAsync(string text)
+        {
+            var data = new {text = text};
+
+            string json = JsonSerializer.Serialize(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://127.0.0.1:8000/echo", content);
+            string responseJson = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<PythonReply>(responseJson);
+            return result.reply;
+        }
+
+    }
+}
